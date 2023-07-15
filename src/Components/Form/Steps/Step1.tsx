8cd4aee4cfa29504aee5f8f6e6ac59109703856d
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2023-07-15 12:05:53 -0300

    [build] Validation step1

--- src/Components/Form/Steps/Step1.tsx
+++ src/Components/Form/Steps/Step1.tsx
@@ -1,16 +1,29 @@
+import React from "react";
+
 const Step1 = () => {
+  const date = new Date();
+  const [dateMin, setDateMin] = React.useState(
+    addDays(date, 1).toISOString().slice(0, 10)
+  );
+
   function addDays(date: Date, days: number) {
     const dateCopy = new Date(date);
     dateCopy.setDate(date.getDate() + days);
     return dateCopy;
   }
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const dateBack = e.target.value;
+  function handleDeparture(e: React.ChangeEvent<HTMLInputElement>) {
+    const dateDeparture = e.target.value;
+    setDateMin(dateDeparture);
   }
 
-  const date = new Date();
-  const dateMin = addDays(date, 1).toISOString().slice(0, 10);
+  function handleBack(e: React.ChangeEvent<HTMLInputElement>) {
+    const dateBack = e.target.value;
+    const departure = new Date(dateMin);
+    console.log(dateMin);
+    // if (date)
+  }
+
   return (
     <div id="step1">
       <label htmlFor="departure">Data de ida:</label>
@@ -20,6 +33,7 @@ const Step1 = () => {
         name="departure"
         id="departure"
         required
+        onChange={handleDeparture}
       />
 
       <label htmlFor="back">Data de volta:</label>
@@ -29,7 +43,7 @@ const Step1 = () => {
         name="back"
         id="back"
         required
-        onChange={handleChange}
+        onChange={handleBack}
       />
     </div>
   );
--- src/Components/Form/Steps/Step3.tsx
+++ src/Components/Form/Steps/Step3.tsx
@@ -2,10 +2,10 @@ const Step3 = () => {
   return (
     <div id="step3">
       <label htmlFor="name">Nome do passageiro responsável:</label>
-      <input type="text" name="name" id="name" />
+      <input type="text" name="name" id="name" required />
 
       <label htmlFor="email">E-mail do passageiro responsável:</label>
-      <input type="email" name="email" id="email" />
+      <input type="email" name="email" id="email" required />
     </div>
   );
 };
